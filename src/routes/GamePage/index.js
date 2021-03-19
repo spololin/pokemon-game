11cd2398a7a796aae185b8f0ddce6e476d64fd13
DESKTOP-U657TGI\LeXX <spololin>
2021-03-19 21:17:22 +0300

    Finish Task3

--- src/routes/GamePage/index.js
+++ src/routes/GamePage/index.js
@@ -1,6 +1,5 @@
 import {useState, useEffect} from 'react';
-// import {useHistory} from 'react-router-dom';
-import styles from "../HomePage/style.module.css";
+import styles from "../GamePage/style.module.css";
 import PokemonCard from "../../components/AppPokemonCard";
 import database from "../../service/firebase";
 
@@ -17,36 +16,53 @@ const GamePage = () => {
         getArrPokemon()
     }, [])
 
-    // const history = useHistory();
-    // const handlerClickButton = () => {
-    //     history.push('/')
-    // }
-
     const handlerClickCard = (id) => {
         const card = arrPokemon.find(elem => elem.id === id);
+        console.log(card)
         card.active = card.hasOwnProperty("active") ? !card.active : true;
         database.ref('pokemons/' + id).set({...card});
-        getArrPokemon()
-        // setArrPokemon(prevState => {
-        //     return Object.entries(prevState).reduce((acc, item) => {
-        //         const pokemon = {...item[1]};
-        //         if (pokemon.id === id) {
-        //             pokemon.active = true;
-        //         }
-        //         acc[item[0]] = pokemon;
-        //         return acc;
-        //     }, {});
-        // });
-        // const idx = arrPokemon.findIndex(card => card.id === id);
-        // const card = arrPokemon[idx];
-        // setArrPokemon([...arrPokemon.slice(0, idx), {...card, active: !card.active}, ...arrPokemon.slice(idx + 1)])
+        getArrPokemon();
+    }
 
+    const handlerClickAddPokemon = () => {
+        const cardNewPokemon = {
+            "abilities": [
+                "static",
+                "lightning-rod"
+            ],
+            "active": false,
+            "base_experience": 112,
+            "height": 4,
+            "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+            "name": "pikachu",
+            "stats": {
+                "attack": 55,
+                "defense": 40,
+                "hp": 35,
+                "special-attack": 50,
+                "special-defense": 50,
+                "speed": 90
+            },
+            "type": "electric",
+            "values": {
+                "bottom": 1,
+                "left": 5,
+                "right": 5,
+                "top": 6
+            },
+            "weight": 60
+        }
+        const newKey = database.ref().child('pokemons').push().key;
+        database.ref('pokemons/' + newKey).set({...cardNewPokemon, id: newKey});
+        getArrPokemon()
     }
 
     return (
         <div>
-            {/*<p>This is Game Page</p>*/}
-            {/*<button onClick={handlerClickButton}>Back to App</button>*/}
+            <div className={styles.addNew}>
+                <button onClick={handlerClickAddPokemon}>Add new pokemon</button>
+            </div>
+
             <div className={styles.flex}>
                 {
                     arrPokemon.map(({id, values, name, type, img, active}) => <PokemonCard
